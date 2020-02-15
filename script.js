2da4021aa0ddc238269433ef2a44116a6a6ad1f5
xaviguasch <xaviguasch@gmail.com>
2020-02-15 22:20:58 +0100

    added selectors in the javascript file

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('it works!!!')
+const wordEl = document.getElementById('word')
+const wrongLettersEl = document.getElementById('wrong-letters')
+const playAgainBtn = document.getElementById('play-button')
+const popup = document.getElementById('popup-container')
+const notification = document.getElementById('notification-container')
+const finalMessage = document.getElementById('final-message')
+
+const figureParts = document.querySelectorAll('.figure-part')

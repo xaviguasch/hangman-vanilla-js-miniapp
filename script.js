853d069c30567687946b1b8779a5784d4a06e07f
xaviguasch <xaviguasch@gmail.com>
2020-02-15 22:23:22 +0100

    selected a random word each within the ones in the array

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
 
 const figureParts = document.querySelectorAll('.figure-part')
+
+const words = ['application', 'programming', 'interface', 'wizard']
+
+let selectedWord = words[Math.floor(Math.random() * words.length)]
